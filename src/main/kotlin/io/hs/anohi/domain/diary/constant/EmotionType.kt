77041ccd593323e1ff2c9fcb060158ad20955cf3
hypernova1 <hypemova@gmail.com>
2023-03-12 18:51:17 +0900

    feat: 감정 이름 이넘 추가

---
+++ src/main/kotlin/io/hs/anohi/domain/diary/constant/EmotionType.kt
@@ -0,0 +1,10 @@
+package io.hs.anohi.domain.diary.constant
+
+enum class EmotionType {
+    HAPPY,
+    GOOD,
+    NOTHING_MUCH,
+    SAD,
+    ANXIOUS,
+    BAD,
+}
--- src/main/kotlin/io/hs/anohi/domain/diary/entity/Emotion.kt
+++ src/main/kotlin/io/hs/anohi/domain/diary/entity/Emotion.kt
@@ -1,19 +1,19 @@
 package io.hs.anohi.domain.diary.entity
 
 import io.hs.anohi.core.BaseEntity
+import io.hs.anohi.domain.diary.constant.EmotionType
 import org.hibernate.annotations.SQLDelete
 import org.hibernate.annotations.Where
 import java.util.*
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.ManyToMany
+import javax.persistence.*
 
 @Entity
 @Where(clause = "deleted_at is null")
 @SQLDelete(sql = "UPDATE emotion SET deleted_at = current_timestamp WHERE id = ?")
 class Emotion: BaseEntity() {
     @Column
-    val name: String = "";
+    @Enumerated(EnumType.STRING)
+    val name: EmotionType = EmotionType.HAPPY;
 
     @ManyToMany(mappedBy = "emotions")
     val diaries: MutableList<Diary> = mutableListOf()
