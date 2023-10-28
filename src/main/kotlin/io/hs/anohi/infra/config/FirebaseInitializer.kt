4850c36a62b1ce6f1a3f99352d097dfc0cfb3f21
hypernova1 <hypemova@gmail.com>
2023-10-28 22:03:12 +0900

    fix: resources 폴더내 파일 못찾는 오류 수정

--- src/main/kotlin/io/hs/anohi/infra/config/FirebaseInitializer.kt
+++ src/main/kotlin/io/hs/anohi/infra/config/FirebaseInitializer.kt
@@ -4,21 +4,21 @@ import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.auth.FirebaseAuth
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ResourceLoader
 import java.io.FileInputStream
 
 @Configuration
 class FirebaseInitializer {
 
+    @Autowired
+    lateinit var resourceLoader: ResourceLoader
+
     @Bean
     fun firebaseApp(): FirebaseApp? {
-        val fileInputStream =
-            this.javaClass.classLoader.getResource("anohinonikki-firebase-adminsdk-yq9nu-df5178cd09.json")?.path?.let {
-                FileInputStream(
-                    it
-                )
-            }
+        val fileInputStream = resourceLoader.getResource("classpath:anohinonikki-firebase-adminsdk-yq9nu-df5178cd09.json").inputStream
         val options = FirebaseOptions.builder()
             .setCredentials(GoogleCredentials.fromStream(fileInputStream))
             .build()
